0036a0946c06ee468c47edeea03a24f1aa9f298e
‘Tim <timothy.j.ocampo@gmail.com>
2019-04-07 15:21:53 -0700

    added heroku cli

--- server.js
+++ server.js
@@ -11,22 +11,23 @@ var app = express();
 //ROUTES
 const contact_us = require('./routes/api/contact-us/submit');
 
-
 //MiddleWare
 app.use(cors());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
-
-app.get('/', function (req, res) {
-    res.send('Hello World')
-});
+//Serves static assets if in prod
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static('build'));
+    app.get('*', (req, res)=>{
+        res.sendFile(path.resolve(__dirname, 'build', 'index.html'))
+    })
+}
 
 // USE ROUTES
 app.use('/api/contact-us/submit', contact_us);
 
-
-let port = 5000;
+const port = process.env.PORT || 5000;
 
 app.listen(port, ()=>{
     console.log(`Welcome to the backend . Listening on port ${port}`)
