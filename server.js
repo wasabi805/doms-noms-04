724f14b20fb6bb3aa433857bf87c36865a3c2f8d
‘Tim <timothy.j.ocampo@gmail.com>
2019-04-07 16:07:40 -0700

    edited server.js to point at dist dir.

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use(bodyParser.json());
 if(process.env.NODE_ENV === 'production'){
     app.use(express.static('build'));
     app.get('*', (req, res)=>{
-        res.sendFile(path.resolve(__dirname, 'build', 'index.html'))
+        res.sendFile(path.resolve(__dirname, 'dist', 'index.html'))
     })
 }
 
