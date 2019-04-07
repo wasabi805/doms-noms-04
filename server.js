8728e5be35f52a107fc31b79b0dbe40164096dd2
‘Tim <timothy.j.ocampo@gmail.com>
2019-04-07 16:13:03 -0700

    had to include dist dir to app.use(express.static) inside server.js

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(bodyParser.json());
 
 //Serves static assets if in prod
 if(process.env.NODE_ENV === 'production'){
-    app.use(express.static('build'));
+    app.use(express.static('dist'));
     app.get('*', (req, res)=>{
         res.sendFile(path.resolve(__dirname, 'dist', 'index.html'))
     })
